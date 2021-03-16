c3a34dfdd88a3dd348ec2cc214964707575c92a5
azzilak <azzilakaren@students.amikom.ac.id>
2021-03-16 14:39:21 +0700

    Add project files.

---
+++ Apps Taxi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps_Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Mas Ojol";
+            taxi.onDutty = true;
+            taxi.numbPassenger = 7;
+
+            taxi.TaxiInfo();
+            taxi.PickUp();
+            taxi.DropOff();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Apps Taxi/Taxi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Apps_Taxi
+{
+    public class Taxi
+    {
+        //PROPERTIES
+        public string DriverName { get; set; }
+        public bool onDutty { get; set; }
+        public float numbPassenger { get; set; }
+
+        //METHOD
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name              : {0}", DriverName);
+
+            if (onDutty == true)
+            {
+                Console.WriteLine("Taxi On Duty             : Yes", onDutty);
+
+            }
+
+            if (onDutty == false)
+            {
+                Console.WriteLine("Taxi On Duty                  : No", onDutty);
+            }
+            
+            Console.WriteLine("Numb. of the Passenger   : {0}", numbPassenger);
+        }
+
+        public void PickUp()
+        {
+            Console.WriteLine("\n {0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOff()
+        {
+            Console.WriteLine("{0} telah selesai mengantar penumpang \n", DriverName);
+        }
+    }
+}
